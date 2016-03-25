56f5fc204902d98eb6084c79c3bbb1537b519efe
Joseph Garcia <j.garc419@gmail.com>
2016-03-25 03:20:29 -0400

    Initial Commit

---
+++ Chipotle Finder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Chipotle Finder
+//
+//  Created by Joseph Garcia on 3/25/16.
+//  Copyright © 2016 joebeard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
